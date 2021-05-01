961df22d60d6dee05449dc5b4c80f4219ec983b6
obeux <1755575607@qq.com>
2021-05-01 13:37:18 +0800

    add O49

---
+++ O49/cmake-build-debug/CMakeFiles/O49.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/O49.dir/main.cpp.o"
+  "O49"
+  "O49.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/O49.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
