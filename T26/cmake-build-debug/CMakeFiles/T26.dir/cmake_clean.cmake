c379366c24c2fe96e9e6192b6e388dfba2ae357a
obeux <1755575607@qq.com>
2021-07-14 09:32:04 +0800

    T26

---
+++ T26/cmake-build-debug/CMakeFiles/T26.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/T26.dir/main.cpp.o"
+  "T26"
+  "T26.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/T26.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
