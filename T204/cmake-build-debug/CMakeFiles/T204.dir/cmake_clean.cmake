339604565ede0ae57ef69f1f325df8e06fc5e0ad
obeux <1755575607@qq.com>
2021-07-16 21:39:49 +0800

    T204

---
+++ T204/cmake-build-debug/CMakeFiles/T204.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/T204.dir/main.cpp.o"
+  "T204"
+  "T204.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/T204.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
