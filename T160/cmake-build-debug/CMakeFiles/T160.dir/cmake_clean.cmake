b2ff49afb02e3a09d4a398ef697f82fe760482e3
obeux <1755575607@qq.com>
2021-07-17 21:16:35 +0800

    T160

---
+++ T160/cmake-build-debug/CMakeFiles/T160.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/T160.dir/main.cpp.o"
+  "T160"
+  "T160.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/T160.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ T160/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+
+    }
+};
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
